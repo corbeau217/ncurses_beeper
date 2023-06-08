a6fc91ec9cf812b223225f6a3e6117e53a1f663d
Corbeau <aurora.griffith@students.mq.edu.au>
2023-06-09 01:55:17 +1000

    yepped
    the initial stuffs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ncurses_beeper
  just a little play around with ncurses
+
+
+
+
+
+## usage
+
+- need to use `cc hello.c -o hello -lncurses` to compile
---
+++ hello.c
@@ -0,0 +1,33 @@
+#include <ncurses.h>
+#define NUMCOUNT 10
+#define NUMOFFSET 48
+
+int main()
+{	
+
+	WINDOW *currWin = initscr();			/* Start curses mode 		  */
+
+
+	// smack em in
+	char thems[getmaxx(currWin)+1];
+	for(int i = 0; i < sizeof(thems); i++){
+		///.... ye
+		//	wrap it in the nums
+		thems[i] = (char)(NUMOFFSET+(i%NUMCOUNT));
+	}
+	// CAP IT
+	thems[sizeof(thems)-1] = '\0';
+
+
+	// print ever line
+	for(int i = 0; i < getmaxy(currWin); i++){
+		//....
+		// currWin
+		printw( thems );
+	}
+	refresh();			/* Print it on to the real screen */
+	getch();			/* Wait for user input */
+	endwin();			/* End curses mode		  */
+
+	return 0;
+}
